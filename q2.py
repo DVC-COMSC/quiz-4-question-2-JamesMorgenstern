4724e86ddc7d943d6b53a6ff5afe767bb87363ac
JamesMorgenstern <jamesklm@comcast.net>
2022-10-03 21:06:26 -0700

    committed1

--- q2.py
+++ q2.py
@@ -1,4 +1,24 @@
+userList = []
+userData = input()
 
+while True:
+    userList.append(userData)
+    userData = input()
+    if (userData == "STOP" or userData == "stop"):
+        break
+
+smallest = userList[0]
+largest = userList[0]
+count = 0
+
+for i in range(len(userList)):
+    if (len(userList[i]) <= len(smallest)):
+        smallest = userList[i]
+
+    if (len(userList[i]) >= len(largest)):
+        largest = userList[i]
+
+print(smallest, largest)
 # ******************************
 # Make your Code
 # ******************************
